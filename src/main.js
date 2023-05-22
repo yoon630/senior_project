bc2def6cb4dce23e550b20ae902a1d6f673cefdc
yoon630 <choiyunseon630@gmail.com>
2023-05-22 21:49:48 +0900

    main.js update

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// 여기 민서꺼 js main.html 참고해서 채우기
+import "./main.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Main from "./main";
+
+const root = ReactDOM.createRoot(document.getElementById("main"));
+root.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
